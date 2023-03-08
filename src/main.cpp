96d60e6d5a2acc10e73b69b12edbdb1fb8779808
adityahari69 <adityahari404@gmail.com>
2023-03-08 09:51:16 +0700

    seting mqtt

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,30 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include <PubSubClient.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+
+// #define MQTT_HOST IPAddress(192, 168, 1, 5)
+
+#define MQTT_PORT 1883
+const char *mqtt_server = "test.mosquitto.org";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE (50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+const char *MQTT_PUB_CO2input = "esp32/mh-z14a/inpit";
+const char *MQTT_PUB_CO2output = "esp32/mh-z14a/output";
+const char *MQTT_PUB_ph = "esp32/dfRobot/ph";
+const char *MQTT_PUB_tbd = "esp32/dfRobot/tbd";
+const char *MQTT_PUB_AirTemp = "esp32/sht3x/temp";
+const char *MQTT_PUB_rh = "esp32/sht3x/rh";
+const char *MQTT_PUB_WatherTemp = "esp32/ds18b20/temp";
 
 #define pwmPin GPIO_NUM_33       // co2 New
 #define pwmPin2 GPIO_NUM_32      // co2 Old
@@ -27,6 +49,59 @@ int co2_Ouput_Contraction;
 static float pHValue;
 float wather_Temp;
 
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1')
+  {
+    digitalWrite(BUILTIN_LED, LOW); // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+  }
+  if ((char)payload[0] == '0')
+  {
+    digitalWrite(BUILTIN_LED, HIGH); // Turn the LED off by making the voltage HIGH
+  }
+}
+
+void reconnect()
+{
+  // Loop until we're reconnected
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str()))
+    {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("codersid/nodemcu/v1", "hello world");
+      // ... and resubscribe
+      client.subscribe("codersid/nodemcu/v1");
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
 void sensor_watherTemp_task(void *Parameters)
 {
   sensors.begin();
@@ -35,7 +110,6 @@ void sensor_watherTemp_task(void *Parameters)
   {
     sensors.requestTemperatures();
     wather_Temp = sensors.getTempCByIndex(0);
-
     vTaskDelay(pdMS_TO_TICKS(3000));
   }
 }
@@ -228,6 +302,26 @@ void keepWiFiAlive(void *parameter)
   }
 }
 
+// void mqttPublish(void *Parameters)
+// {
+
+//   while (true)
+//   {
+//     if (!client.connected())
+//     {
+//       reconnect();
+//       Serial.println("reconnecting");
+//     }
+//     client.loop();
+
+//     snprintf(msg, MSG_BUFFER_SIZE, "co2 New #%ld", (const char *)co2_Input_Contraction);
+//     Serial.print("Publish message: ");
+//     Serial.println(msg);
+//     client.publish("codersid/nodemcu/v1", msg);
+//   }
+//   vTaskDelay(pdMS_TO_TICKS(3000));
+// }
+
 void setup()
 {
   // initialize LEDC and other setup code
@@ -280,10 +374,39 @@ void setup()
       1,                        // Task priority
       NULL,                     // Task handle
       1);
+
+  // xTaskCreatePinnedToCore(
+  //     mqttPublish,
+  //     "mqttPublish", // Task name
+  //     5000,          // Stack size (bytes)
+  //     NULL,          // Parameter
+  //     1,             // Task priority
+  //     NULL,          // Task handle
+  //     0);
+
+  // Connect to the MQTT broker
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 }
 void loop()
 {
-  // other loop code
-  prinDataSensor();
-  delay(9000);
+  // // other loop code
+  // prinDataSensor();
+  // delay(9000);
+
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  client.loop();
+
+  unsigned long now = millis();
+  if (now - lastMsg > 2000)
+  {
+    lastMsg = now;
+    snprintf(msg, MSG_BUFFER_SIZE, "co2 New #%ld", (const char *)co2_Input_Contraction);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("codersid/nodemcu/v1", msg);
+  }
 }
